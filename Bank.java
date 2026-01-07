1c7e5ef2708c1fd7629ed6c7018fdd55d4f49812
SonuNigam7782 <sonun7782@gmail.com>
2026-01-07 18:40:58 +0530

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -3,44 +3,52 @@ class BankAccount {
     int accountNumber;
     String holderName;
     double balance;
+
+
     BankAccount() {
         this.accountNumber = 0;
         this.holderName = "Not Assigned";
         this.balance = 0;
-        
-        BankAccount(int accountNumber, String holderName, double balance) {
+    }
+
+    
+    BankAccount(int accountNumber, String holderName, double balance) {
         this.accountNumber = accountNumber;
         this.holderName = holderName;
         this.balance = balance;
     }
+
+
     void deposit(double amount) {
         this.balance = this.balance + amount;
         System.out.println("Amount deposited: " + amount);
     }
-        
     void withdraw(double amount) {
-        if (amount <= this.balance) {this.balance = this.balance - amount;
+        if (amount <= this.balance) {
+            this.balance = this.balance - amount;
             System.out.println("Amount withdrawn: " + amount);
         } else {
             System.out.println("Insufficient balance");
         }
     }
+
+  
     void displayDetails() {
         System.out.println("Account Number: " + this.accountNumber);
         System.out.println("Holder Name: " + this.holderName);
         System.out.println("Balance: " + this.balance);
     }
 }
-    
-class Bank {
+
+public class Bank {
     public static void main(String[] args) {
 
         BankAccount acc1 = new BankAccount();
         acc1.displayDetails();
 
         System.out.println();
-        
-        BankAccount acc2 = new BankAccount(101, "Rahul", 5000);
+
+        BankAccount acc2 = new BankAccount(101, "Sonu", 5000);
         acc2.displayDetails();
 
         acc2.deposit(3000);
@@ -50,4 +58,3 @@ class Bank {
         acc2.displayDetails();
     }
 }
-
